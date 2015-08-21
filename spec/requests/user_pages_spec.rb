4646a90be280f868d1556eee25da7d09eec5ed8d
himorikiko <withoutmosk@gmail.com>
2015-08-22 00:56:04 +0700

    Add some exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,7 +29,11 @@ describe "User pages" do
 
     describe "follow/unfollow buttons" do
       let(:other_user) { FactoryGirl.create(:user) }
-      before { sign_in user }
+      before do
+        sign_in user
+        other_user.follow!(user)
+        visit user_path(user)
+      end
 
       describe "following a user" do
         before { visit user_path(other_user) }
@@ -75,6 +79,9 @@ describe "User pages" do
           it { should have_xpath("//input[@value='Follow']") }
         end
       end
+
+      it { should have_link("0 following", href: following_user_path(user)) }
+      it { should have_link("1 followers", href: followers_user_path(user)) }
     end
   end
 
